a6670fa3e04a39f62aa3c16ae5bb7f35c105eb62
palak-maheshwari <53858389+palak-maheshwari@users.noreply.github.com>
2020-03-31 20:35:33 +0530

    Update MyPriorityQueue.java

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -1,3 +1,4 @@
+
 /*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
@@ -6,5 +7,19 @@
  */
 package problem3.myqueue;
 
+import problem3.myLinkedList.MyLinkedList;
+import problem3.student.Student;
+
 public class MyPriorityQueue {
+    MyLinkedList linkedList = new MyLinkedList();
+    public void enqueue(Student student){
+        linkedList.addLast(student);
+    }
+
+    public void dequeue(){
+        linkedList.deleteFirst();
+    }
+    public void show(){
+        linkedList.display();
+    }
 }
